dcf488cfd36a544fb988f02ec6649fd3b6e43c24
masashi yoshikawa <earnest.marshi@gmail.com>
2019-12-22 22:08:54 +0900

    json serialize trait

--- toid/src/lib.rs
+++ toid/src/lib.rs
@@ -2,8 +2,10 @@
 //! パフォーマンス向上のため、toidはrustで実装されていますが、
 //! 通常はPython3などのスクリプト言語にバインディングして使うことを想定しています。
 
+pub mod collaboration;
 pub mod music_state_manager;
 pub mod portaudio_outputter;
+pub mod serialize;
 pub mod state_management;
 pub mod states;
 
