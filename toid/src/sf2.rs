ce1391f21b3d3f1430a249d11be2d6be990acc84
masashi yoshikawa <earnest.marshi@gmail.com>
2019-12-27 23:14:26 +0900

    fix for warning

--- toid/src/sf2.rs
+++ toid/src/sf2.rs
@@ -6,11 +6,9 @@ use toid::data::riff;
 fn main() {
     let mut f = File::open("../SGM-V2.01.sf2").unwrap();
     let mut buffer = Vec::new();
-    f.read_to_end(&mut buffer);
+    f.read_to_end(&mut buffer).unwrap();
     let buffer = buffer.as_slice();
 
-    let parsed = riff::RiffChank::parse(buffer);
-    if let Ok((_, chank)) = parsed {
-        chank.print();
-    }
+    let (_, chank) = riff::RiffChank::parse(buffer).expect("Failed to parse RIFF");
+    chank.print();
 }
