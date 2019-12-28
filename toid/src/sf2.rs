90b3ade218d4e5c8c89d2a43d2e48e6084bdec2b
masashi yoshikawa <earnest.marshi@gmail.com>
2019-12-28 12:29:07 +0900

    remove print chank

--- toid/src/sf2.rs
+++ toid/src/sf2.rs
@@ -10,9 +10,6 @@ fn main() {
     f.read_to_end(&mut buffer).unwrap();
     let buffer = buffer.as_slice();
 
-    let chank = riff::RiffChank::parse(buffer);
-    println!("{}", chank);
-
     let sf2_data = sf2::SF2::parse(buffer);
     println!("{}", sf2_data);
 }
