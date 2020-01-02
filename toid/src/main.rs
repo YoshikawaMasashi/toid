62964d4dfb3be9c0870e3dbc9a239306b2c84339
masashi yoshikawa <earnest.marshi@gmail.com>
2020-01-02 15:58:37 +0900

    update main

--- toid/src/main.rs
+++ toid/src/main.rs
@@ -1,54 +1,66 @@
 extern crate portaudio;
 
-use std::boxed::Box;
-use std::fs::File;
-use std::io::Read;
 use std::sync::Arc;
-use std::sync::RwLock;
 
-use toid::data::sf2;
-use toid::music_state_manager::{MusicStateEvent, MusicStateManager};
+use toid::new_music_store::melody_state::MelodyStateEvent;
+use toid::new_music_store::new_music_store::NewMusicStore;
+use toid::new_music_store::sf2_state::SF2StateEvent;
+use toid::new_music_store::wave_reader::WaveReader;
 use toid::portaudio_outputter::PortAudioOutputter;
-use toid::state_management::store::Store;
-use toid::states::music_state::MusicState;
-use toid::stores::default_store::DefaultStore;
 
 fn main() {
-    let store: Box<dyn Store<MusicState>> = Box::new(DefaultStore::new(MusicState::new()));
-    let store = Arc::new(RwLock::new(store));
+    let store = NewMusicStore::new();
+    let store = Arc::new(store);
 
-    let sound_state_manager = MusicStateManager::new(Arc::clone(&store));
-    let sound_state_manager = Arc::new(RwLock::new(sound_state_manager));
+    let wave_reader = WaveReader::new(Arc::clone(&store));
+    let wave_reader = Arc::new(wave_reader);
 
-    let mut portaudio_outputter = PortAudioOutputter::new(Arc::clone(&sound_state_manager));
-    {
-        let reducer = sound_state_manager.read().unwrap().get_reducer();
+    let mut portaudio_outputter = PortAudioOutputter::new(Arc::clone(&wave_reader));
 
-        let mut f = File::open("../florestan-subset.sf2").unwrap();
-        let mut buffer = Vec::new();
-        f.read_to_end(&mut buffer).unwrap();
-        let buffer = buffer.as_slice();
-        let sf2 = sf2::own::SF2::parse(buffer);
-        let sf2 = Arc::new(sf2);
+    store
+        .sf2
+        .update_state(SF2StateEvent::LoadAndSetSF2(String::from(
+            "../florestan-subset.sf2",
+        )));
 
-        reducer.reduce(MusicStateEvent::SetSF2(sf2));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOn(48.0, 0 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOn(50.0, 1 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOn(52.0, 2 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOn(53.0, 3 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOn(55.0, 4 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOn(57.0, 6 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOn(53.0, 7 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOn(52.0, 8 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOff(9 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOn(50.0, 10 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOff(11 * (44100 / 4)));
+    store
+        .melody
+        .update_state(MelodyStateEvent::AddNewNoteOn(48.0, 12 * (44100 / 4)));
 
-        portaudio_outputter.run();
-
-        reducer.reduce(MusicStateEvent::AddNewNoteOn(60.0 - 12.0, 0 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOn(62.0 - 12.0, 1 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOn(64.0 - 12.0, 2 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOn(65.0 - 12.0, 3 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOn(67.0 - 12.0, 4 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOn(69.0 - 12.0, 6 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOn(65.0 - 12.0, 7 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOn(64.0 - 12.0, 8 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOff(9 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOn(62.0 - 12.0, 10 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOff(11 * (44100 / 4)));
-        reducer.reduce(MusicStateEvent::AddNewNoteOn(60.0 - 12.0, 12 * (44100 / 4)));
-    }
-
+    portaudio_outputter.run();
     portaudio_outputter.sleep(4000);
     portaudio_outputter.stop();
 }
--- toid/src/portaudio_outputter.rs
+++ toid/src/portaudio_outputter.rs
@@ -1,41 +1,42 @@
 extern crate portaudio;
 
-use super::music_state_manager::MusicStateManager;
+// use super::music_state_manager::MusicStateManager;
+use super::new_music_store::wave_reader::WaveReader;
+use super::new_state_management::store_reader::StoreReader;
 use portaudio as pa;
 use std::option::Option;
 use std::sync::Arc;
-use std::sync::RwLock;
 
 const CHANNELS: i32 = 2;
 const SAMPLE_RATE: f64 = 44_100.0;
 const FRAMES_PER_BUFFER: u32 = 512;
 
 pub struct PortAudioOutputter {
-    sound_state_manager: Arc<RwLock<MusicStateManager>>,
+    wave_reader: Arc<WaveReader>,
     portaudio: pa::PortAudio,
-    stream: Option<Arc<RwLock<pa::Stream<pa::NonBlocking, pa::Output<i16>>>>>,
+    stream: Option<pa::Stream<pa::NonBlocking, pa::Output<i16>>>,
 }
 
 impl PortAudioOutputter {
-    pub fn new(sound_state_manager: Arc<RwLock<MusicStateManager>>) -> Self {
+    pub fn new(wave_reader: Arc<WaveReader>) -> Self {
         let portaudio = pa::PortAudio::new().unwrap();
 
         PortAudioOutputter {
-            sound_state_manager,
+            wave_reader,
             portaudio,
             stream: None,
         }
     }
 
     pub fn run(&mut self) {
-        let sound_state_manager = Arc::clone(&self.sound_state_manager);
+        let wave_reader = Arc::clone(&self.wave_reader);
         let callback = move |pa::OutputStreamCallbackArgs::<'static, i16> {
                                  buffer,
                                  frames,
                                  ..
                              }|
               -> pa::StreamCallbackResult {
-            let waves = sound_state_manager.write().unwrap().get_wave();
+            let waves = wave_reader.read();
 
             let mut idx = 0;
             for i in 0..frames {
@@ -58,22 +59,12 @@ impl PortAudioOutputter {
             .unwrap();
 
         stream.start().unwrap();
-        self.stream = Some(Arc::new(RwLock::new(stream)));
+        self.stream = Some(stream);
     }
 
     pub fn stop(&mut self) {
-        Option::as_ref(&self.stream)
-            .unwrap()
-            .write()
-            .unwrap()
-            .stop()
-            .unwrap();
-        Option::as_ref(&self.stream)
-            .unwrap()
-            .write()
-            .unwrap()
-            .close()
-            .unwrap();
+        Option::as_mut(&mut self.stream).unwrap().stop().unwrap();
+        Option::as_mut(&mut self.stream).unwrap().close().unwrap();
         self.stream = None;
     }
 
