eb18b6d1d9b678d12c572707baa1570a85f0ee00
masashi yoshikawa <earnest.marshi@gmail.com>
2020-01-02 19:48:50 +0900

    change some

--- toid/src/music_store/wave_reader.rs
+++ toid/src/music_store/wave_reader.rs
@@ -31,7 +31,8 @@ impl StoreReader<NewMusicStore, Vec<i16>> for WaveReader {
     }
 
     fn read(&self) -> Vec<i16> {
-        let mut ret = Vec::new();
+        let mut ret: Vec<i16> = Vec::new();
+        ret.resize(self.wave_length as usize, 0);
 
         let melody_state = self.store.melody.get_state();
         let scheduling_state = self.store.scheduling.get_state();
@@ -42,7 +43,9 @@ impl StoreReader<NewMusicStore, Vec<i16>> for WaveReader {
         let event_seq = melody_state.event_seq.clone();
         let mut current_melody = melody_state.current_melody.clone();
 
-        for current_samples in current_cumulative_samples..next_cumulative_samples {
+        for (wave_idx, current_samples) in
+            (current_cumulative_samples..next_cumulative_samples).enumerate()
+        {
             let current_samples = match melody_state.repeat_length {
                 Some(repeat_length) => {
                     (current_samples - melody_state.repeat_start) % repeat_length
@@ -62,7 +65,7 @@ impl StoreReader<NewMusicStore, Vec<i16>> for WaveReader {
                 },
             };
 
-            let ret_ = match current_melody {
+            let addition = match current_melody {
                 CurrentMelodyState::On(pitch, samples) => {
                     if let Some(sf2) = &sf2_state.sf2 {
                         sf2.get_sample(0, pitch as u8, samples as usize)
@@ -72,9 +75,9 @@ impl StoreReader<NewMusicStore, Vec<i16>> for WaveReader {
                         (x.sin() * 30000.0) as i16
                     }
                 }
-                CurrentMelodyState::Off => 0,
+                _ => 0,
             };
-            ret.push(ret_);
+            ret[wave_idx] = ret[wave_idx].saturating_add(addition);
         }
 
         match current_melody {
