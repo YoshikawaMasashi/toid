43c57096e71d1d070326ad5b91b35a867db5429f
marshi <earnest.marshi@gmail.com>
2020-04-19 23:11:32 +0900

    websocker player binding

--- pytoid/src/lib.rs
+++ pytoid/src/lib.rs
@@ -2,14 +2,18 @@ use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
 
 mod outputters;
 mod players;
+mod sf2;
 
 use outputters::portaudio_outputter::PortAudioOutputter;
 use players::local_player::LocalPlayer;
+use players::websocket_player::{WebSocketPlayer, WebSocketPlayerServer};
 
 #[pymodule]
 fn toid(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_class::<LocalPlayer>()?;
     m.add_class::<PortAudioOutputter>()?;
+    m.add_class::<LocalPlayer>()?;
+    m.add_class::<WebSocketPlayer>()?;
+    m.add_class::<WebSocketPlayerServer>()?;
 
     Ok(())
 }
