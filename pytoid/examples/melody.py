c067f147b84b5a9d7166157c2757f8cfa730c3f3
marshi <earnest.marshi@gmail.com>
2020-04-19 23:35:39 +0900

    setitem

--- pytoid/examples/melody.py
+++ pytoid/examples/melody.py
@@ -10,8 +10,10 @@ if __name__ == "__main__":
 
     player.set_sf2_name("sf2.test")
 
-    player.send_num_lang("12345 643 2 1", 0.0, "main",)
-    player.send_num_lang("1   4   5   1", -1.0, "sub",)
+    # player.send_num_lang("12345 643 2 1", 0.0, "main",)
+    # player.send_num_lang("1   4   5   1", -1.0, "sub",)
+    player['main'] = "12345 643 2 1"
+    player['sub'] = "1   4   5   1", -1.0
 
     portaudio_outputter.run()
     time.sleep(12)
--- pytoid/toid/__init__.py
+++ pytoid/toid/__init__.py
@@ -1,2 +1,38 @@
-from .toid import LocalPlayer, PortAudioOutputter, WebSocketPlayer, WebSocketPlayerServer  # NOQA
+from .toid import PortAudioOutputter, WebSocketPlayer, WebSocketPlayerServer  # NOQA
 from . import sf2  # NOQA
+
+from . import toid
+
+
+class LocalPlayer(object):
+    def __init__(self):
+        self.player = toid.LocalPlayer()
+
+    def set_sf2_name(self, name):
+        self.player.set_sf2_name(name)
+
+    def send_num_lang(self, melody_string, octave, name):
+        self.player.send_num_lang(melody_string, octave, name)
+
+    def resource_register(self, path):
+        self.player.resource_register(path)
+    
+    def load_sf2(self, name):
+        self.player.load_sf2(name)
+
+    def get_toid_player(self):
+        return self.player.get_toid_player()
+
+    def __setitem__(self, key, value):
+        if isinstance(key, str):
+            if isinstance(value, tuple):
+                if len(value) == 2:
+                    self.send_num_lang(value[0], value[1], key)
+                else:
+                    raise Exception("invalid value")
+            elif isinstance(value, str):
+                self.send_num_lang(value, 0.0, key)
+            else:
+                raise Exception("invalid value")
+        else:
+            raise Exception("invalid value")
