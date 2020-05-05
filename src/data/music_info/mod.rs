01a6572ef78ee5091fba28197fa50576bbc5af59
masashi yoshikawa <earnest.marshi@gmail.com>
2020-05-05 19:16:25 +0900

    marge and delay

--- src/data/music_info/mod.rs
+++ src/data/music_info/mod.rs
@@ -1,3 +1,7 @@
 pub mod beat;
 pub mod note;
 pub mod phrase;
+
+pub use beat::Beat;
+pub use note::Note;
+pub use phrase::Phrase;
--- src/high_layer_trial/phrase_operation/mod.rs
+++ src/high_layer_trial/phrase_operation/mod.rs
@@ -1,1 +1,43 @@
+use std::cmp;
+use std::collections::BTreeMap;
 
+use super::super::data::music_info::{Beat, Note, Phrase};
+
+pub fn delay(phrase: Phrase, delay: Beat) -> Phrase {
+    let mut new_notes = BTreeMap::new();
+    for (&start, note_vec) in phrase.notes.iter() {
+        let mut new_note_vec = vec![];
+        for note in note_vec.iter() {
+            new_note_vec.push(Note {
+                pitch: note.pitch,
+                duration: note.duration,
+                start: note.start + delay,
+            });
+        }
+        new_notes.insert(start + delay, new_note_vec);
+    }
+
+    let new_repeat_length = phrase.repeat_length + delay;
+
+    Phrase {
+        notes: new_notes,
+        repeat_length: new_repeat_length,
+    }
+}
+
+pub fn marge(phrase1: Phrase, phrase2: Phrase) -> Phrase {
+    let mut new_phrase = Phrase::new();
+    new_phrase =
+        new_phrase.set_repeat_length(cmp::max(phrase1.repeat_length, phrase2.repeat_length));
+    for (_, note_vec) in phrase1.notes.iter() {
+        for &note in note_vec.iter() {
+            new_phrase = new_phrase.add_note(note);
+        }
+    }
+    for (_, note_vec) in phrase2.notes.iter() {
+        for &note in note_vec.iter() {
+            new_phrase = new_phrase.add_note(note);
+        }
+    }
+    new_phrase
+}
