721afcd0519761b4afef67feaac697280627e6db
masashi yoshikawa <earnest.marshi@gmail.com>
2020-05-05 19:23:10 +0900

    change key

--- src/high_layer_trial/phrase_operation/mod.rs
+++ src/high_layer_trial/phrase_operation/mod.rs
@@ -64,3 +64,25 @@ pub fn concat(phrase1: Phrase, phrase2: Phrase) -> Phrase {
 
     new_phrase
 }
+
+pub fn change_key(phrase: Phrase, key: f32) -> Phrase {
+    let mut new_notes = BTreeMap::new();
+    for (&start, note_vec) in phrase.notes.iter() {
+        let mut new_note_vec = vec![];
+        for note in note_vec.iter() {
+            new_note_vec.push(Note {
+                pitch: note.pitch + key,
+                duration: note.duration,
+                start: note.start,
+            });
+        }
+        new_notes.insert(start, new_note_vec);
+    }
+
+    let new_length = phrase.length;
+
+    Phrase {
+        notes: new_notes,
+        length: new_length,
+    }
+}
