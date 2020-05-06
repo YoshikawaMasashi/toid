24021459d4dfe0e6c219fc946b1aa14d2a9b14da
masashi yoshikawa <earnest.marshi@gmail.com>
2020-05-06 19:02:48 +0900

    shuffle_start, invert_start_order, invert_pitch

--- src/high_layer_trial/phrase_operation/mod.rs
+++ src/high_layer_trial/phrase_operation/mod.rs
@@ -1,6 +1,9 @@
 use std::cmp;
 use std::collections::BTreeMap;
 
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 use super::super::data::music_info::{Beat, Note, Phrase};
 
 pub fn delay(phrase: Phrase, delay: Beat) -> Phrase {
@@ -85,4 +88,59 @@ pub fn change_key(phrase: Phrase, key: f32) -> Phrase {
         notes: new_notes,
         length: new_length,
     }
+}
+
+pub fn shuffle_start(phrase: Phrase) -> Phrase {
+    let mut new_phrase = Phrase::new();
+    new_phrase = new_phrase.set_length(phrase.length);
+
+    let mut new_starts: Box<[Beat]> = phrase.notes.keys().cloned().collect();
+    let mut rng = thread_rng();
+    new_starts.shuffle(&mut rng);
+    for ((_, note_vec), &new_start) in phrase.notes.iter().zip(new_starts.iter()) {
+        for note in note_vec.iter() {
+            new_phrase.add_note(Note {
+                pitch: note.pitch,
+                duration: note.duration,
+                start: new_start,
+            });
+        }
+    }
+
+    new_phrase
+}
+
+pub fn invert_start_order(phrase: Phrase) -> Phrase {
+    let mut new_phrase = Phrase::new();
+    new_phrase = new_phrase.set_length(phrase.length);
+
+    let new_starts = phrase.notes.keys().rev();
+    for ((_, note_vec), &new_start) in phrase.notes.iter().zip(new_starts) {
+        for note in note_vec.iter() {
+            new_phrase.add_note(Note {
+                pitch: note.pitch,
+                duration: note.duration,
+                start: new_start,
+            });
+        }
+    }
+
+    new_phrase
+}
+
+pub fn invert_pitch(phrase: Phrase, center: f32) -> Phrase {
+    let mut new_phrase = Phrase::new();
+    new_phrase = new_phrase.set_length(phrase.length);
+
+    for (_, note_vec) in phrase.notes.iter() {
+        for note in note_vec.iter() {
+            new_phrase.add_note(Note {
+                pitch: center - (note.pitch - center),
+                duration: note.duration,
+                start: note.start,
+            });
+        }
+    }
+
+    new_phrase
 }
