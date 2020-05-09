70f67297d00767608ea095d555eeb7f4c76b3d77
masashi yoshikawa <earnest.marshi@gmail.com>
2020-05-09 16:16:38 +0900

    pitch to from string

--- src/data/music_info/pitch.rs
+++ src/data/music_info/pitch.rs
@@ -1,7 +1,58 @@
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::ops::Sub;
 
 use serde::{Deserialize, Serialize};
+
+lazy_static! {
+    static ref STRING_TO_F32_PITCH: HashMap<String, f32> = {
+        let mut map = HashMap::new();
+        for octave_idx in 0..9 {
+            map.insert(format!("C{}", octave_idx), octave_idx as f32 * 12.0 + 12.0);
+            map.insert(format!("C#{}", octave_idx), octave_idx as f32 * 12.0 + 13.0);
+            map.insert(format!("Db{}", octave_idx), octave_idx as f32 * 12.0 + 13.0);
+            map.insert(format!("D{}", octave_idx), octave_idx as f32 * 12.0 + 14.0);
+            map.insert(format!("D#{}", octave_idx), octave_idx as f32 * 12.0 + 15.0);
+            map.insert(format!("Eb{}", octave_idx), octave_idx as f32 * 12.0 + 15.0);
+            map.insert(format!("E{}", octave_idx), octave_idx as f32 * 12.0 + 16.0);
+            map.insert(format!("F{}", octave_idx), octave_idx as f32 * 12.0 + 17.0);
+            map.insert(format!("F#{}", octave_idx), octave_idx as f32 * 12.0 + 18.0);
+            map.insert(format!("Gb{}", octave_idx), octave_idx as f32 * 12.0 + 18.0);
+            map.insert(format!("G{}", octave_idx), octave_idx as f32 * 12.0 + 19.0);
+            map.insert(format!("G#{}", octave_idx), octave_idx as f32 * 12.0 + 20.0);
+            map.insert(format!("Ab{}", octave_idx), octave_idx as f32 * 12.0 + 20.0);
+            map.insert(format!("A{}", octave_idx), octave_idx as f32 * 12.0 + 21.0);
+            map.insert(format!("A#{}", octave_idx), octave_idx as f32 * 12.0 + 22.0);
+            map.insert(format!("Bb{}", octave_idx), octave_idx as f32 * 12.0 + 22.0);
+            map.insert(format!("B{}", octave_idx), octave_idx as f32 * 12.0 + 23.0);
+        }
+        map
+    };
+
+    static ref I32_PITCH_TO_PITCH_NAME: HashMap<i32, String> = {
+        let mut map = HashMap::new();
+        for octave_idx in 0..9 {
+            map.insert(octave_idx as i32 * 12 + 12, format!("C{}", octave_idx));
+            // map.insert(octave_idx as i32 * 12 + 13, format!("C#{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 13, format!("Db{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 14, format!("D{}", octave_idx));
+            // map.insert(octave_idx as i32 * 12 + 15, format!("D#{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 15, format!("Eb{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 16, format!("E{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 17, format!("F{}", octave_idx));
+            // map.insert(octave_idx as i32 * 12 + 18, format!("F#{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 18, format!("Gb{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 19, format!("G{}", octave_idx));
+            // map.insert(octave_idx as i32 * 12 + 20, format!("G#{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 20, format!("Ab{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 21, format!("A{}", octave_idx));
+            // map.insert(octave_idx as i32 * 12 + 22, format!("A#{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 22, format!("Bb{}", octave_idx));
+            map.insert(octave_idx as i32 * 12 + 23, format!("B{}", octave_idx));
+        }
+        map
+    };
+}
 
 #[derive(Serialize, Deserialize, Clone, Copy, Debug)]
 pub struct Pitch {
@@ -84,6 +135,20 @@ impl From<i32> for Pitch {
         Pitch {
             pitch: pitch as f32,
         }
+    }
+}
+
+impl From<String> for Pitch {
+    fn from(pitch_name: String) -> Self {
+        Pitch {
+            pitch: STRING_TO_F32_PITCH[&pitch_name],
+        }
+    }
+}
+
+impl Into<String> for Pitch {
+    fn into(self) -> String {
+        I32_PITCH_TO_PITCH_NAME[&(self.pitch as i32)].clone()
     }
 }
 
