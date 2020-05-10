d6d83bbbfde15e57057bd8cf9c943cc15537b3d2
masashi yoshikawa <earnest.marshi@gmail.com>
2020-05-10 19:31:37 +0900

    drums done

--- examples/sample_phrase.rs
+++ examples/sample_phrase.rs
@@ -1,8 +1,10 @@
 use std::sync::Arc;
 
-use toid::data::music_info::beat::Beat;
+use toid::data::music_info::{Beat, SampleNote, SamplePhrase, SampleTrack};
 use toid::high_layer_trial::music_language::num_lang::send_num_lang;
-use toid::music_state::states::{MusicState, MusicStateEvent, SchedulingStateEvent};
+use toid::music_state::states::{
+    MusicState, MusicStateEvent, SchedulingStateEvent, SectionStateEvent,
+};
 use toid::music_state::wave_reader::{WaveReader, WaveReaderEvent};
 use toid::outputters::portaudio_outputter::PortAudioOutputter;
 use toid::players::local_player::LocalPlayer;
@@ -15,6 +17,10 @@ fn main() {
     player
         .get_resource_manager()
         .register(String::from("./toid-sample-resource/sf2/sf2.toml"))
+        .unwrap();
+    player
+        .get_resource_manager()
+        .register(String::from("./toid-sample-resource/drums/drums.toml"))
         .unwrap();
 
     let mut portaudio_outputter = PortAudioOutputter::new(Arc::clone(&player)
@@ -94,6 +100,25 @@ fn main() {
     )
     .unwrap();
 
+    let mut sample_phrase = SamplePhrase::new();
+    sample_phrase = sample_phrase.add_note(SampleNote {
+        sound: "x".to_string(),
+        start: Beat::from(0),
+    });
+    sample_phrase = sample_phrase.set_length(Beat::from(1));
+    let sample_track = SampleTrack {
+        phrase: sample_phrase,
+        sample_name: "example_drums".to_string(),
+        vol: 1.0,
+        pan: 0.0,
+    };
+    player
+        .send_event(MusicStateEvent::SectionStateEvent(
+            Beat::from(0),
+            SectionStateEvent::NewSampleTrack("drums".to_string(), sample_track),
+        ))
+        .unwrap();
+
     send_num_lang(
         "5 3 4 65        ".to_string(),
         0.0,
@@ -137,6 +162,29 @@ fn main() {
             >,
     )
     .unwrap();
+
+    let mut sample_phrase = SamplePhrase::new();
+    sample_phrase = sample_phrase.add_note(SampleNote {
+        sound: "x".to_string(),
+        start: Beat::from(0),
+    });
+    sample_phrase = sample_phrase.add_note(SampleNote {
+        sound: "o".to_string(),
+        start: Beat::from(0),
+    });
+    sample_phrase = sample_phrase.set_length(Beat::from(1));
+    let sample_track = SampleTrack {
+        phrase: sample_phrase,
+        sample_name: "example_drums".to_string(),
+        vol: 1.0,
+        pan: 0.0,
+    };
+    player
+        .send_event(MusicStateEvent::SectionStateEvent(
+            Beat::from(8),
+            SectionStateEvent::NewSampleTrack("drums".to_string(), sample_track),
+        ))
+        .unwrap();
 
     portaudio_outputter.run().unwrap();
     portaudio_outputter.sleep(2250);
--- src/music_state/states/section.rs
+++ src/music_state/states/section.rs
@@ -2,13 +2,14 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
-use super::super::super::data::music_info::Track;
+use super::super::super::data::music_info::{SampleTrack, Track};
 use super::super::super::state_management::serialize;
 use super::super::super::state_management::state::State;
 
 #[derive(Serialize, Deserialize)]
 pub struct SectionState {
     pub track_map: HashMap<String, Track>,
+    pub sample_track_map: HashMap<String, SampleTrack>,
 }
 
 impl SectionState {
@@ -17,6 +18,16 @@ impl SectionState {
         new_track_map.insert(key, track);
         Self {
             track_map: new_track_map,
+            sample_track_map: self.sample_track_map.clone(),
+        }
+    }
+
+    fn new_sample_track(&self, key: String, track: SampleTrack) -> Self {
+        let mut new_sample_track_map = self.sample_track_map.clone();
+        new_sample_track_map.insert(key, track);
+        Self {
+            track_map: self.track_map.clone(),
+            sample_track_map: new_sample_track_map,
         }
     }
 }
@@ -25,12 +36,14 @@ impl State<SectionStateEvent> for SectionState {
     fn new() -> Self {
         Self {
             track_map: HashMap::new(),
+            sample_track_map: HashMap::new(),
         }
     }
 
     fn reduce(&self, event: SectionStateEvent) -> Self {
         match event {
             SectionStateEvent::NewTrack(key, track) => self.new_track(key, track),
+            SectionStateEvent::NewSampleTrack(key, track) => self.new_sample_track(key, track),
         }
     }
 }
@@ -53,6 +66,7 @@ impl serialize::Serialize<SectionState> for SectionState {
 #[derive(Serialize, Deserialize)]
 pub enum SectionStateEvent {
     NewTrack(String, Track),
+    NewSampleTrack(String, SampleTrack),
 }
 
 impl serialize::Serialize<SectionStateEvent> for SectionStateEvent {
