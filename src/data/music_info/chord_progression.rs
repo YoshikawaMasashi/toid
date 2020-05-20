a25f65a6bd342fc4d7ddb4bab8533c2b1176b6bb
masashi yoshikawa <earnest.marshi@gmail.com>
2020-05-20 20:02:56 +0900

    update chord progression

--- src/data/music_info/chord_progression.rs
+++ src/data/music_info/chord_progression.rs
@@ -1,26 +1,20 @@
-use std::collections::BTreeMap;
-
 use serde::{Deserialize, Serialize};
 
 use super::{Beat, Chord};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct ChordProgression {
-    chords_map: BTreeMap<Beat, Chord>,
+    chords: Vec<(Chord, Beat)>,
 }
 
 impl ChordProgression {
     pub fn new() -> Self {
-        ChordProgression {
-            chords_map: BTreeMap::new(),
-        }
+        ChordProgression { chords: Vec::new() }
     }
 
-    pub fn add_chord(&self, start: Beat, chord: Chord) -> Self {
-        let mut new_chords_map = self.chords_map.clone();
-        new_chords_map.insert(start, chord);
-        ChordProgression {
-            chords_map: new_chords_map,
-        }
+    pub fn add_chord(&self, chord: Chord, duration: Beat) -> Self {
+        let mut new_chords = self.chords.clone();
+        new_chords.push((chord, duration));
+        ChordProgression { chords: new_chords }
     }
 }
