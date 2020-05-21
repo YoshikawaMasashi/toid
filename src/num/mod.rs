045ecc62bd756f24c934191e6b6dac9b11e4a309
masashi yoshikawa <earnest.marshi@gmail.com>
2020-05-21 23:05:22 +0900

    normalize

--- src/num/mod.rs
+++ src/num/mod.rs
@@ -17,3 +17,33 @@ pub fn parlin_noise_seq(size: usize, degree: f32, seed: Option<f32>) -> Vec<f32>
 
     noise
 }
+
+pub fn max_min_normalize(vec: &Vec<f32>) -> Vec<f32> {
+    let mut new_vec = vec![];
+
+    let max = vec.iter().fold(0.0 as f32, |max, &i| max.max(i));
+    let min = vec.iter().fold(0.0 as f32, |min, &i| min.min(i));
+    let diff = max - min;
+
+    for &v in vec.iter() {
+        new_vec.push((v - min) / diff);
+    }
+
+    new_vec
+}
+
+pub fn change_max_min(vec: &Vec<f32>, new_max: f32, new_min: f32) -> Vec<f32> {
+    let mut new_vec = vec![];
+
+    let max = vec.iter().fold(0.0 as f32, |max, &i| max.max(i));
+    let min = vec.iter().fold(0.0 as f32, |min, &i| min.min(i));
+    let diff = max - min;
+
+    let new_diff = new_max - new_min;
+
+    for &v in vec.iter() {
+        new_vec.push((v - min) / diff * new_diff + new_min);
+    }
+
+    new_vec
+}
