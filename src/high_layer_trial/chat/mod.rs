84b7976ae50c4a172aff6f8d249f8d2909cbc685
masashi yoshikawa <earnest.marshi@gmail.com>
2020-08-19 20:52:36 +0900

    update

--- src/high_layer_trial/chat/mod.rs
+++ src/high_layer_trial/chat/mod.rs
@@ -6,11 +6,121 @@ use nom::character::complete::{char, one_of, digit1};
 use nom::combinator::{iterator, not};
 use nom::IResult;
 
-use super::super::data::music_info::{Beat, Phrase, SampleNote};
+use super::super::data::music_info::{Beat, Phrase, PitchNote, PitchInterval, Pitch};
 use super::super::music_state::states::{MusicState, MusicStateEvent};
 use super::super::music_state::wave_reader::{WaveReader, WaveReaderEvent};
 use super::super::players::player::Player;
 use super::music_language::send_phrase::send_sample_phrase;
+use super::phrase_operation::change_key;
+
+fn parse_phrase(s: &str) -> IResult<&str, Phrase<PitchNote>> {
+    let mut it = iterator(s, one_of("+-"));
+    let pm_chrs: Vec<char> = it.collect();
+    let (s, _) = it.finish()?;
+    let mut octave: i32 = 0;
+    for pm_chr in pm_chrs.iter() {
+        match pm_chr {
+            '+' => {
+                octave += 1;
+            },
+            '-' => {
+                octave -= 1;
+            },
+            _ => {}
+        }
+    }
+
+    let (s, elements) = parse_elements(s)?;
+
+    let mut phrase = Phrase::new();
+    phrase = phrase.set_length(Beat::from(elements.len() as f32 / 2.0));
+
+    let mut now: Beat = Beat::from(0);
+    let length_unit: Beat = Beat::from(0.5);
+
+    for element in elements.iter() {
+        match element {
+            Element::Pitch(pitch_element) => {
+                let pitch = match pitch_element.pitch % 7 {
+                    0 => 47.0,
+                    1 => 48.0,
+                    2 => 50.0,
+                    3 => 52.0,
+                    4 => 53.0,
+                    5 => 55.0,
+                    6 => 57.0,
+                    _ => unreachable!(),
+                };
+                let pitch = pitch + (pitch_element.pitch / 7) as f32 * 12.0;
+                let pitch = pitch + pitch_element.shift as f32;
+
+                let note = PitchNote {
+                    pitch: Pitch::from(pitch),
+                    start: now,
+                    duration: length_unit,
+                };
+                phrase = phrase.add_note(note);
+            }
+            Element::Tuplet(tuplet_element) => {
+                let notes = tuplet_to_notes(tuplet_element.clone(), now, length_unit);
+                for note in notes.iter() {
+                    phrase = phrase.add_note(note.clone());
+                }
+            }
+            Element::Rest() => {
+            }
+        }
+        now = now + length_unit;
+    }
+
+    phrase = change_key(phrase, PitchInterval::from(12.0 * octave as f32));
+
+    Ok((s, phrase))
+}
+
+
+fn tuplet_to_notes(tuplet_element: TupletElement, start: Beat, duration: Beat) -> Vec<PitchNote> {
+    let length_unit: Beat = duration / tuplet_element.size() as f32;
+    let mut now = start;
+    let mut ret_notes = vec![];
+    // TODO
+    for element in tuplet_element.elements.iter() {
+        match element {
+            Element::Pitch(pitch_element) => {
+                let pitch = match pitch_element.pitch % 7 {
+                    0 => 47.0,
+                    1 => 48.0,
+                    2 => 50.0,
+                    3 => 52.0,
+                    4 => 53.0,
+                    5 => 55.0,
+                    6 => 57.0,
+                    _ => unreachable!(),
+                };
+                let pitch = pitch + (pitch_element.pitch / 7) as f32 * 12.0;
+                let pitch = pitch + pitch_element.shift as f32;
+
+                let note = PitchNote {
+                    pitch: Pitch::from(pitch),
+                    start: now,
+                    duration: length_unit,
+                };
+                ret_notes.push(note);
+            }
+            Element::Rest() => {
+
+            }
+            Element::Tuplet(tuplet_element) => {
+                let notes = tuplet_to_notes(tuplet_element.clone(), now, length_unit);
+                for note in notes.iter() {
+                    ret_notes.push(note.clone());
+                }
+            }
+        }
+        now = now + length_unit;
+    }
+    ret_notes
+}
 
 #[derive(Clone, Debug, PartialEq)]
 enum Element {
